fbcbad5d62b455edc37790f03bd6a88993f3ee6f
kanowory <kanowory@student.wszib.edu.pl>
2023-06-04 09:43:38 +0200

    Initial commit

---
+++ src/test/java/pl/edu/wszib/jwd/colors/ColorsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.jwd.colors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
